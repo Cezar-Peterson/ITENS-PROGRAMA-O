ac68f8a25dba849b0bfbb9ad14c764c69ee062b3
Cezar-Peterson <71391270+Cezar-Peterson@users.noreply.github.com>
2021-04-29 08:15:55 -0300

    Update EXE_BRUNO_15_03_2021.sql

--- BRUNO/AULA 15_03/EXE_BRUNO_15_03_2021.sql
+++ BRUNO/AULA 15_03/EXE_BRUNO_15_03_2021.sql
@@ -2,6 +2,7 @@ create database exe_bruno_15_03_2021;
 
 -- exe1-----------------------------------------
 
+-- criar database e table
 use exe_bruno_15_03_2021;
 create table conta(
 	idconta int primary key auto_increment,
@@ -14,6 +15,7 @@ create table conta(
     sexo char(1)
 );
 
+-- inserts
 insert into conta(nome, sobrenome, celular, email, senha, dt_nascimento, sexo) values("cezar", "peterson", "48 99998888"
 ,"cezar@gmail.com", "123456789", "2004-08-05", "m");
 
@@ -22,25 +24,31 @@ insert into conta(nome, sobrenome, celular, email, senha, dt_nascimento, sexo) v
 
 insert into conta(nome, sobrenome, celular, email, senha, dt_nascimento, sexo) values("jonata", "silva", "48 15987423"
 ,"jonata@gmail.com", "258741369", "2004-3-30", "m");
+
+-- select da table
 SELECT * FROM exe_bruno_15_03_2021.conta;
 
 -- exe2-----------------------------------------
 
+-- create table
 create table escola(
 	idaluno int primary key auto_increment,
     nome varchar(100)
 );
-
+-- inserts
 insert into escola(nome) values("rafael");
 insert into escola(nome) values("cezar");
 insert into escola(nome) values("sofia");
 insert into escola(nome) values("marcia");
 insert into escola(nome) values("jonata");
 insert into escola(nome) values("emili");
+
+-- select da table
 SELECT * FROM exe_bruno_15_03_2021.escola;
 
 -- exe3-----------------------------------------
 
+-- criar table
 create table estacionamento(
 	idcarro int primary key auto_increment,
     marca varchar(100),
@@ -52,17 +60,22 @@ create table estacionamento(
     numero_do_chassi char(17)
 );
 
+-- insert
 insert into estacionamento(marca, modelo, ano_de_fabricacao, ano_do_modelo, placa, cor, numero_do_chassi)
 values ("chevrolet", "opala", 1979, 1979, "abc1234", "azul xingu", "4BC5R87HX24060831");
 
 insert into estacionamento(marca, modelo, ano_de_fabricacao, ano_do_modelo, placa, cor, numero_do_chassi)
 values ("chevrolet", "opala", 1992, 1992, "abc1235", "azul gythion", "9BGVN15FGHB000001");
 
+-- deletar idcarro = 3
 delete from estacionamento where idcarro = 3;
 
+-- select da table
 SELECT * FROM exe_bruno_15_03_2021.estacionamento;
 
 -- exe4-----------------------------------------
+
+-- criar table
 create table biblioteca(
 	idlivro int primary key auto_increment,
     titulo varchar(100),
@@ -71,8 +84,11 @@ create table biblioteca(
     ano_de_publicacao int
 );
 
+-- inserts
 insert into biblioteca(titulo, autor, editora, ano_de_publicacao) values("as aventuras de cezar", "cezar", "saraiva", 2021);
 insert into biblioteca(titulo, autor, editora, ano_de_publicacao) values("joao, o maniaco", "jeremias", "alguma coisa", 2021);
+
+-- select da table
 SELECT * FROM exe_bruno_15_03_2021.biblioteca;
 
 
